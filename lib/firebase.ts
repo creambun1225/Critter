b63849efa60ef004cc1cb57bfef8bf2fbe762e45
creambun <creambun1225@gmail.com>
2026-03-23 08:55:41 +0900

    update

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,17 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
+// Firebase設定
 const firebaseConfig = {
-  apiKey: "ここはそのまま",
-  authDomain: "ここもそのまま",
-  projectId: "ここもそのまま",
-  storageBucket: "ここもそのまま",
-  messagingSenderId: "ここもそのまま",
-  appId: "ここもそのまま"
+  apiKey: "AIzaSyC3v8B5z-WuhhC4sI8c1s6FUDk2NYX_WmY",
+  authDomain: "critter-3c31b.firebaseapp.com",
+  projectId: "critter-3c31b",
+  storageBucket: "critter-3c31b.firebasestorage.app",
+  messagingSenderId: "154125669854",
+  appId: "1:154125669854:web:9b411bb1e77f151d8a794f"
 };
 
+// 初期化
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+// 🔥 これが超重要
 export const auth = getAuth(app);
+export const db = getFirestore(app);
