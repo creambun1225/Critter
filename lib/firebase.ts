b05d46b49a3d26bd1b0b2dcd454db6d58a8fdc2a
creambun <creambun1225@gmail.com>
2026-05-11 22:20:54 +0900

    add modal post

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,18 +1,51 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import {
+  initializeApp
+} from "firebase/app";
+
+import {
+  getAuth
+} from "firebase/auth";
+
+import {
+  getFirestore
+} from "firebase/firestore";
+
+import {
+  getStorage
+} from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3v8B5z-WuhhC4sI8c1s6FUDk2NYX_WmY",
-  authDomain: "critter-3c31b.firebaseapp.com",
-  projectId: "critter-3c31b",
-  storageBucket: "critter-3c31b.firebasestorage.app",
-  messagingSenderId: "154125669854",
-  appId: "1:154125669854:web:9b411bb1e77f151d8a794f",
-  measurementId: "G-BN7RJ4MY32"
+
+  apiKey:
+    "AIzaSyC3v8B5z-WuhhC4sI8c1s6FUDk2NYX_WmY",
+
+  authDomain:
+    "critter-3c31b.firebaseapp.com",
+
+  projectId:
+    "critter-3c31b",
+
+  storageBucket:
+    "critter-3c31b.firebasestorage.app",
+
+  messagingSenderId:
+    "154125669854",
+
+  appId:
+    "1:154125669854:web:9b411bb1e77f151d8a794"
+
 };
 
-const app = initializeApp(firebaseConfig);
+const app =
+  initializeApp(
+    firebaseConfig
+  );
 
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const auth =
+  getAuth(app);
+
+export const db =
+  getFirestore(app);
+
+export const storage =
+  getStorage(app);
