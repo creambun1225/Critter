ccc150b638c67a2ef79efd35f668ff1f7bb98678
creambun <creambun1225@gmail.com>
2026-05-11 17:45:07 +0900

    add modal post

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,3 +1,34 @@
-export default function PostCard() {
-  return <div>POST CARD</div>;
+"use client";
+
+export default function PostCard({
+  post,
+  currentUser
+}: any) {
+
+  return (
+
+    <div className="border-b border-zinc-800 p-4 text-white">
+
+      <div className="font-bold text-xl">
+
+        {post.name}
+
+      </div>
+
+      <div className="text-zinc-500 mb-3">
+
+        @{post.username}
+
+      </div>
+
+      <div>
+
+        {post.text}
+
+      </div>
+
+    </div>
+
+  );
+
 }
